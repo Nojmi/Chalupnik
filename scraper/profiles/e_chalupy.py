77d48694e3c0602068a18b59541f9c238f4508a7
Nojmi <neumtomas@gmail.com>
2026-07-27 14:04:09 +0200

    fix: realističtější HTTP hlavičky pro e-chalupy.cz

--- scraper/profiles/e_chalupy.py
+++ scraper/profiles/e_chalupy.py
@@ -30,7 +30,11 @@ SOURCE_NAME = "e-chalupy.cz"
 BASE_URL = "https://e-chalupy.cz"
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (compatible; ChalupnikBot/1.0; +https://github.com/Nojmi/Chalupnik)"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Language": "cs-CZ,cs;q=0.9,en;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
 }
 
 CAPACITY_RE = re.compile(r"(\d+)\s*osob")
